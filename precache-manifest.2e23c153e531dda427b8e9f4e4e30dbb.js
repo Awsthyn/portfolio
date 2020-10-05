3673b1849b8eb4963b065e66c7bfbd407475e5b1
Awsthyn <aguswagner008@gmail.com>
2020-10-04 21:35:30 -0300

    Updates

---
+++ precache-manifest.2e23c153e531dda427b8e9f4e4e30dbb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "925f57a326c6fe7dffc654613bb70197",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1ed4aff56ce70a420d27",
+    "url": "/portfolio/static/css/main.82d299ed.chunk.css"
+  },
+  {
+    "revision": "2d52bafaa59b71085625",
+    "url": "/portfolio/static/js/2.ca76b177.chunk.js"
+  },
+  {
+    "revision": "df73ff29913fe38884585241c864145a",
+    "url": "/portfolio/static/js/2.ca76b177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed4aff56ce70a420d27",
+    "url": "/portfolio/static/js/main.112dafda.chunk.js"
+  },
+  {
+    "revision": "309a9e55c61ba5bff665",
+    "url": "/portfolio/static/js/runtime-main.b3df5f95.js"
+  },
+  {
+    "revision": "eb3233be91acf572e1b813932a7b4119",
+    "url": "/portfolio/static/media/avatar.eb3233be.png"
+  },
+  {
+    "revision": "e04353bf94ac5c9fefb9482329bb3476",
+    "url": "/portfolio/static/media/whiteboard.e04353bf.png"
+  }
+]);
